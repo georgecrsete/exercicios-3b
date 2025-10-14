986018eb55f132c0d040bf6fa8094682d33e877c
George <george.p@escolar.ifrn.edu.br>
2025-10-14 13:47:37 -0300

    kk

--- exercicios3b/6/6.2.py
+++ exercicios3b/6/6.2.py
@@ -1,18 +1,16 @@
-# @cikey 67674417a027ae17ee1925a4537fb74e
-# @sid 20251174010012
-# @aid V6.2
+import random
+def adivinha():
+    numero_secreto = random.randint(1, 10)
 
+    while True:
+        n = int(input())
 
-#begin_inputs
-n = int(input())
-#end_inputs
+        if n < numero_secreto:
+            print("maior")
+        elif n > numero_secreto:
+            print("menor")
+        else:
+            print("o numero secreto era", numero_secreto)
+            break
 
-
-
-
-def faca_numeros(n):
-    for i in range(1, n + 1):
-        for j in range(1, i + 1):
-            print(j, end=' ')
-        print()
-faca_numeros(n)
+adivinha()
