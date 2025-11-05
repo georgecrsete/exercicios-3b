ed06890d3beeacf08d5863dc558319f2cc31f832
George <george.p@escolar.ifrn.edu.br>
2025-11-05 17:43:29 -0300

    upp

--- exercicios3b/8/8.2.py
+++ exercicios3b/8/8.2.py
@@ -1,14 +1,17 @@
 # @cikey 67674417a027ae17ee1925a4537fb74e
 # @sid 20251174010012
-# @aid V8.1
+# @aid V8.2
+
+
 def escrever_arquivo():
     with open('text.t', "w") as arquivo:
         frase = input("digite uma frase: ")
-        arquivo.write(frase)
-def ler_arquivo():
+        for i in range(5):
+            arquivo.write(frase)
+def contar_arquivo():
     with open("text.t",'r') as arquivo:
-        conteudo = arquivo.read()
-        print("Conteúdo do arquivo:", conteudo)
-
+       linhas = arquivo.readlines()
+    quantidade = len(linhas)
+    print(f"O arquivo tem {quantidade} linhas.")
 escrever_arquivo()
-ler_arquivo()
+contar_arquivo()
