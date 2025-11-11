a03339e3d9288037c7bd99113327e8acf312fd43
George <george.p@escolar.ifrn.edu.br>
2025-11-11 14:05:56 -0300

    up 8

--- exercicios3b/8/8.3.py
+++ exercicios3b/8/8.3.py
@@ -1,3 +1,9 @@
+# @cikey 67674417a027ae17ee1925a4537fb74e
+# @sid 20251174010012
+# @aid V8.3
+
+
+
 def ler_arquivo():
     with open("frase.txt",'r') as arquivo:
         conteudo = arquivo.read()
