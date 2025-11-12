fbff07e4978f93896a2d513750c90c683b451317
George <george.p@escolar.ifrn.edu.br>
2025-11-12 17:48:18 -0300

    kk

--- exercicios3b/8/8.5.py
+++ exercicios3b/8/8.5.py
@@ -2,3 +2,17 @@
 # @sid 20251174010012
 # @aid V8.5
 
+
+def contatos():
+    with open('contatos.txt', "a") as arquivo:
+        for i in range(3):
+            nome = input("Digite o nome: ")
+            telefone = input("Digite o telefone: ")
+            arquivo.write(nome +";"+ telefone + "\n")
+        print("contato salvo com sucesso!")
+
+    with open("contatos.txt", "r") as arquivo:
+        print("\nContatos cadastrados:")
+        for linha in arquivo:
+            print(linha.strip())
+contatos()
