0e3a1d0d6319c16b686a1430e87505e37ff6b308
George <george.p@escolar.ifrn.edu.br>
2025-11-26 17:29:40 -0300

    upp

--- exercicios3b/8/8.3.py
+++ exercicios3b/8/8.3.py
@@ -5,13 +5,13 @@
 
 
 def ler_arquivo():
-    with open("frase83.txt",'r') as arquivo:
+    with open("frase.txt",'r') as arquivo:
         conteudo = arquivo.read()
     return conteudo
 
 def copia_arquivo():
     conteudo = ler_arquivo()
-    with open("frasecopia83.txt",'w') as copiaarquivo:
+    with open("frasecopia.txt",'w') as copiaarquivo:
         copiaarquivo.write(conteudo)
 ler_arquivo()
 copia_arquivo()
